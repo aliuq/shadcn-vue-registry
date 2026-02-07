8ee1389d23d95b77dfefad223f3cc0300d8d59d8
aliuq <aliuq@bilishare.com>
2026-02-07 19:53:29 +0000

    feat: update ESLint configuration and pnpm workspace for @antfu/eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,9 +13,4 @@ export default antfu({
   rules: {
     'pnpm/json-enforce-catalog': 0,
   },
-}, {
-  files: ['packages/examples/**/*.vue', 'packages/examples/**/*.ts'],
-  rules: {
-    'no-console': 'off',
-  },
 })

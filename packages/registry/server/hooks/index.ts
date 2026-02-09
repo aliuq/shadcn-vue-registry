944b1d26eaa19d2c2ebc3fac101ce1f07bebb704
aliuq <aliuq@bilishare.com>
2026-02-09 03:33:19 +0000

    chore: patch vercel build error

--- packages/registry/server/hooks/index.ts
+++ packages/registry/server/hooks/index.ts
@@ -1,10 +1,17 @@
-import type { Nitro } from 'nitro/types'
+import type { Nitro, NitroHooks } from 'nitro/types'
 import { generateRegistryAssets } from '../utils/registryBuilder'
 
-export const buildHooks = {
+export const buildHooks: Partial<NitroHooks> = {
   'build:before': async (nitro: Nitro) => {
     await generateRegistryAssets({
       rootDir: nitro.options.rootDir,
     })
+  },
+  'rollup:before': async (nitro: Nitro, config) => {
+    // Patch vercel build error:
+    // Cannot resolve entry module xxxxx/nitro/dist/presets/vercel/runtime/vercel.{format}.
+    if (nitro.options.preset === 'vercel' && config.input && typeof config.input === 'string') {
+      config.input = config.input.replace('{format}', 'node.mjs')
+    }
   },
 }
